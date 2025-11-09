8785de4b6190ba278628d0b488394b5786ad12ee
Prathamesh2603 <prathameshpawar645@gmail.com>
2025-11-09 15:48:50 +0530

    all flag codes are added and flag img changes according to flag code

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+const BASE_URL = "";
 
+const selects = document.querySelectorAll("select");
+
+for (const select of selects) {
+    for (const currCode in countryList) {
+        let newOption = document.createElement("option");
+        newOption.innerText = currCode;
+        newOption.value = currCode;
+        if (select.id === "from-select-country" && currCode === "USD") {
+            newOption.selected = "selected";
+        }
+        else if (select.id === "to-select-country" && currCode === "INR") {
+            newOption.selected = "selected";
+        }
+        select.appendChild(newOption);
+    }
+
+    select.addEventListener("change", (evt) => {
+        updateFlag(evt.target);
+    });
+}
+
+const updateFlag = (element) => {
+    let currCode = element.value;
+    let countryCode = countryList[currCode];
+    let newSrc = `https://flagsapi.com/${countryCode}/flat/24.png`
+    let img = element.parentElement.querySelector("img");
+    img.src = newSrc;
+}
