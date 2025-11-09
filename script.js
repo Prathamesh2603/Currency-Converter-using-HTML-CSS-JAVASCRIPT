2b234e6823478b97fe0bf28263c9a8f9dc8b1c2c
Prathamesh2603 <prathameshpawar645@gmail.com>
2025-11-09 18:06:56 +0530

    Currency conversion is done. whole project is done

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-const BASE_URL = "";
+const BASE_URL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies";
 
 const selects = document.querySelectorAll("select");
+const convertBtn = document.querySelector("button");
+const fromCurr = document.querySelector("#from-select-country");
+const toCurr = document.querySelector("#to-select-country");
+const toinput = document.querySelector("#to-input");
 
 for (const select of selects) {
     for (const currCode in countryList) {
@@ -28,3 +32,31 @@ const updateFlag = (element) => {
     let img = element.parentElement.querySelector("img");
     img.src = newSrc;
 }
+
+convertBtn.addEventListener("click", () => {
+    updateExchangeRate();
+});
+
+let updateExchangeRate = async () => {
+    let amount = document.querySelector("#from-input");
+    let amtVal = amount.value;
+    if (amtVal === "" || amtVal < 1) {
+        amtVal = 1;
+        amount.value = "1";
+    }
+    
+    const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}.json`;
+    let response = await fetch(URL);
+    let data = await response.json();
+    let rate;
+    for (const key in data[fromCurr.value.toLowerCase()]) {
+        if (toCurr.value.toLowerCase() === key) {
+            rate = data[fromCurr.value.toLowerCase()][key];
+        }       
+    }
+    let finalVal = amtVal * rate;
+    toinput.value = finalVal;
+}
+window.addEventListener("load", () => {
+    updateExchangeRate();
+});
